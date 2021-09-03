04446b082a97f39986114633feb00734bedfb0a7
Katie <btvgiahoang@gmail.com>
2021-09-03 20:54:18 +0930

    Initial Commit

---
+++ CodingMentor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingMentor
+//
+//  Created by Mac on 9/3/21.
+//  Copyright © 2021 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
