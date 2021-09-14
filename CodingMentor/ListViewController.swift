5dd3fdf4cac7fef5e453453c6f030acae7d9caeb
Katie <btvgiahoang@gmail.com>
2021-09-14 19:54:30 +0930

    delete list feature

--- CodingMentor/ListViewController.swift
+++ CodingMentor/ListViewController.swift
@@ -126,6 +126,19 @@ extension ListViewController: UITableViewDataSource {
         cell.textLabel?.text = tasks[indexPath.row]
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+        }
+    }
 }
 
 extension ListViewController: UITextFieldDelegate {
